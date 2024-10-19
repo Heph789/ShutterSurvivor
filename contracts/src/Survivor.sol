b4800b1b2243365c00bb8c4d701330024287d792
chase <chaselbjeter@gmail.com>
2024-10-19 16:54:13 -0700

    Early iteration of contract

--- contracts/src/Survivor.sol
+++ contracts/src/Survivor.sol
@@ -2,6 +2,8 @@
 pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
 
 contract Survivor is AccessControl {
 
@@ -18,6 +20,7 @@ contract Survivor is AccessControl {
     mapping(bytes32 => Game) public activeGames;
     event GameCreated(bytes32 indexed id, uint8 num_addresses);
     event VoteCast(bytes32 indexed gameId, address caster, bytes32 commitment);
+    event PlayerRemoved(bytes32 indexed gameId, address removedPlayer);
 
     // note: make sure to store addresses in a sorted order
     function createGame(string calldata name, address[] memory addresses) external {
@@ -47,20 +50,35 @@ contract Survivor is AccessControl {
 
     // I'm not actually sure what structure is best for sigs
     // Ideally, here is where you would get the revealed commitments on chain to get the sigs, and all you would have to pass are the addresses
-    function removeAddress(bytes32 gameId, bytes32[] calldata sigs, address[] calldata addresses, address[] calldata votes) external onlyRole(gameId) {
+    function removePlayer(bytes32 gameId, bytes[] calldata sigs, address[] calldata addresses, address[] calldata votes) external onlyRole(gameId) {
         require(sigs.length == addresses.length && sigs.length == activeGames[gameId].roundCommits.length && sigs.length == votes.length, "Lengths don't match");
-        address toEliminate = addresses[0];
+        address toEliminate = votes[0];
         uint8 threshold = 0;
         for (uint i = 0; i < sigs.length; i++) {
-            require(_verifySig(sigs[i], addresses[i], keccak256(abi.encodePacked(votes[i]))), "Invalid signature");
+            require(_verifySig(sigs[i], addresses[i], votes[i]), "Invalid signature");
             activeGames[gameId].eliminationVotes[votes[i]] += 1;
             if (activeGames[gameId].eliminationVotes[votes[i]] > threshold) {
                 toEliminate = votes[i];
+                threshold = activeGames[gameId].eliminationVotes[votes[i]];
             }
+            // reset the voted addresses
+            activeGames[gameId].addressStatuses[addresses[i]] = AddressStatus.READY;
         }
+        
+        // reset the round
+        for (uint i = 0; i < votes.length; i++) {
+            activeGames[gameId].eliminationVotes[votes[i]] = 0;
+        }
+
+        activeGames[gameId].roundCommits = new bytes32[](0);
+        activeGames[gameId].nextRoundEnd = 0;
+
+        // remove the player
+        activeGames[gameId].addressStatuses[toEliminate] = AddressStatus.ELIMINATED;
+        emit PlayerRemoved(gameId, toEliminate);
     }
 
-    function _verifySig(bytes32 sig, address add, bytes32 dataHash) internal pure returns(bool) {
-
+    function _verifySig(bytes calldata sig, address add, address data) internal pure returns(bool) {
+        return ECDSA.recover(MessageHashUtils.toEthSignedMessageHash(abi.encodePacked(data)), sig) == add;
     }
 }
