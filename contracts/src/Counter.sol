87eb305cd9166b6a8403662233f3df2a3db8fa5c
chase <chaselbjeter@gmail.com>
2024-10-19 13:59:56 -0700

    Initial progress.

--- contracts/src/Counter.sol
+++ contracts/src/Counter.sol
@@ -1,14 +1,29 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Counter {
-    uint256 public number;
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+contract Survivor is AccessControl {
+
+    struct Game {
+        address[] addresses;
+        bytes32[] roundCommits;
+        uint256 nextRoundEnd;
     }
 
-    function increment() public {
-        number++;
+    mapping(bytes32 => Game) public activeGames;
+    event GameCreated(bytes32 indexed id, uint8 num_addresses);
+
+    // note: make sure to store addresses in a sorted order
+    function createGame(string calldata name, address[] memory addresses) external {
+        bytes32 id = keccak256(abi.encodePacked(name));
+        activeGames[id] = Game(addresses, new bytes32[](0), 0);
+        _grantRole(id, msg.sender); // game admin
+        emit GameCreated(id, uint8(addresses.length));
+    }
+
+    function startRound(bytes32 gameId, uint256 endTime) external onlyRole(gameId) {
+        require(activeGames[gameId].nextRoundEnd != 0, "Round already in progress");
+        activeGames[gameId].nextRoundEnd = endTime;
     }
 }
